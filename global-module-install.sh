38a52340de29af94493c98e1e1efb74151d863be
Christopher B Flesche <chris@fletch22.com>
2017-07-23 18:16:53 -0500

    removed old file; updated mac install shell script.

--- global-module-install.sh
+++ global-module-install.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-npm install bower@1.7.7 -g
-npm install browser-sync@2.11.1 -g
-npm install gulp@3.9.1 -g
-npm install karma@0.13.21 -g
-npm install node-gyp@3.3.0 -g
-npm install nodemon@1.9.1 -g
-npm install pangyp@2.3.3 -g
-npm install phantomjs-prebuilt@2.1.4 -g
-npm install webpack@1.12.14 -g
-npm install webpack-dev-server@1.14.1 -g
+npm uninstall bower@1.7.7 \
+browser-sync@2.11.1 gulp@3.9.1 karma@0.13.21 \
+node-gyp@3.3.0 nodemon@1.9.1 pangyp@2.3.3 phantomjs-prebuilt@2.1.4 \
+webpack@1.12.14 webpack-dev-server@1.14.1 -g
 
+npm install bower@1.7.7 \
+browser-sync@2.11.1 gulp@3.9.1 karma@0.13.21 \
+node-gyp@3.3.0 nodemon@1.9.1 pangyp@2.3.3 phantomjs-prebuilt@2.1.4 \
+webpack@1.12.14 webpack-dev-server@1.14.1 -g
 
 
 #rm -rf ./node_modules/
 #
-#npm install
-#
-#npm start
+
+npm install
+
+npm start
