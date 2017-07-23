72eaf2df165d6ea66087e818534ba132db23892d
Christopher B Flesche <chris@fletch22.com>
2017-07-23 12:55:01 -0500

    Fixed mac install by removing lots of globals.

--- tools-extract.sh
+++ tools-extract.sh
@@ -1,54 +1,18 @@
 #!/usr/bin/env bash
 
-#npm i lodash eslint eslint-plugin-jsx-a11y eslint-plugin-import eslint-plugin-react eslint-config-airbnb babel-eslint bower gulp\
-# karma nodemon node-gyp browser-sync node-sass pangyp phantomjs-prebuilt sass-lint webpack webpack-dev-server -g
-
-npm uninstall lodash eslint eslint-plugin-jsx-a11y eslint-plugin-import eslint-plugin-react eslint-config-airbnb babel-eslint bower gulp karma nodemon node-gyp browser-sync node-sass pangyp phantomjs-prebuilt sass-lint webpack webpack-dev-server -g
-
-npm uninstall babel-eslint -g
-npm uninstall babel-plugin-transform-decorators-legacy -g
-npm uninstall bower -g
-npm uninstall browser-sync -g
-npm uninstall eslint -g
-npm uninstall eslint-config-airbnb@6.0.2
-npm uninstall eslint-config-rackt@1.1.1
-npm uninstall eslint-plugin-react@4.1.0
-npm uninstall generator-react-slick@0.3.0
-npm uninstall gulp -g
-npm uninstall jasmine -g
-npm uninstall karma -g
-npm uninstall node-gyp -g
-npm uninstall node-sass -g
-npm uninstall nodemon -g
-npm uninstall pangyp -g
-npm uninstall phantomjs-prebuilt -g
-npm uninstall react-bootstrap -g
-npm uninstall react-dom -g
-npm uninstall sass-lint -g
-npm uninstall webpack -g
-npm uninstall webpack-dev-server -g
-npm uninstall yo -g
-
-npm install babel-eslint@5.0.0 -g
-npm install babel-plugin-transform-decorators-legacy@1.3.4 -g
 npm install bower@1.7.7 -g
 npm install browser-sync@2.11.1 -g
-npm install eslint@2.2.0 -g
-npm install eslint-config-airbnb@6.0.2 -g
-npm install eslint-config-rackt@1.1.1 -g
-npm install eslint-plugin-react@4.1.0 -g
-npm install generator-react-slick@0.3.0 -g
 npm install gulp@3.9.1 -g
-npm install jasmine@2.4.1 -g
 npm install karma@0.13.21 -g
 npm install node-gyp@3.3.0 -g
-npm install node-sass@3.4.2 -g
 npm install nodemon@1.9.1 -g
 npm install pangyp@2.3.3 -g
 npm install phantomjs-prebuilt@2.1.4 -g
-npm install react-bootstrap@0.28.5 -g
-npm install react-dom@15.0.1 -g
-npm install sass-lint@1.5.0 -g
 npm install webpack@1.12.14 -g
 npm install webpack-dev-server@1.14.1 -g
-npm install yo@1.8.5 -g
+
+#rm -rf ./node_modules/
+#
+#npm install
+#
+#npm start
