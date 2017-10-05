991e055bdbeb48c642022a786e147ebfbdc537aa
Christopher B Flesche <chris@fletch22.com>
2017-10-04 20:59:40 -0500

    fixed last(?) dnd problem; added comments

--- app/js/actions/dnd/DndActionHandler.js
+++ app/js/actions/dnd/DndActionHandler.js
@@ -11,6 +11,9 @@ class DndActionHandler {
     const draggedItem = graphTraversal.find(stateNew, draggedId);
     const parentOfDraggedItem = graphTraversal.find(stateNew, draggedItem.parentId);
     const indexCurrentDraggedItem = graphTraversal.getChildsIndex(parentOfDraggedItem.viewModel.children, dragNDrop.draggedId);
+
+    // NOTE: 10-03-2017: If we are dragging a new item or if the dragged item's parent is different or if the ordinal position of the
+    // dragged item has changed
     if (draggedId !== dragNDrop.draggedId
     || (draggedItem.parentId !== parentOfDraggedItem.id || dragNDrop.indexDraggedItem !== indexCurrentDraggedItem)) {
       dragNDrop.draggedId = draggedId;
@@ -21,27 +24,42 @@ class DndActionHandler {
 
     let parentOfHoverOver = null;
 
-
-
-
-
-
+    // NOTE: 10-03-2017: If the thing we are hovering over is new, or the position has changed
     if (hoverOveredId !== dragNDrop.hoverOverId || position !== dragNDrop.position) {
       dragNDrop.hoverOverId = hoverOveredId;
 
       dragNDrop.lastChildViewModelId = null;
       if (position === 'middle') {
+        // NOTE: 10-03-2017: The 'middle' position is possible only in containers that can be dropped into.
+        // So change make the parent the drop target.
         dragNDrop.parentOfHoverOverId = hoverOveredId;
         parentOfHoverOver = graphTraversal.find(stateNew, dragNDrop.parentOfHoverOverId);
+
+        // NOTE: 10-03-2017: Make the last child the ordinal drop position.
         dragNDrop.indexChildTarget = parentOfHoverOver.viewModel.children.length;
+
+        // NOTE: 10-03-2017: Is there more than one child?
         if (parentOfHoverOver.viewModel.children.length > 0) {
+          // NOTE: 10-03-2017: Does the hovered over component come after the dragged component and are they in the same container?
           if (dragNDrop.indexChildTarget > dragNDrop.indexDraggedItem && dragNDrop.parentOfDraggedItemId === dragNDrop.parentOfHoverOverId) {
             dragNDrop.indexChildTarget++;
           }
+
+          // NOTE: 10-03-2017: Pretend the container is empty.
           dragNDrop.lastChildViewModelId = parentOfHoverOver.viewModel.children[parentOfHoverOver.viewModel.children.length - 1].id;
+
+          // NOTE: 10-03-2017: fleschec: Dragging over hidden dragged.
+          if (dragNDrop.lastChildViewModelId === dragNDrop.draggedId) {
+            // NOTE: 10-03-2017: Pretend the container is empty.
+            if (parentOfHoverOver.viewModel.children.length === 1) {
+              dragNDrop.lastChildViewModelId = null;
+            } else {
+              // NOTE: 10-03-2017: Otherwise use the previous component.
+              dragNDrop.lastChildViewModelId = parentOfHoverOver.viewModel.children[parentOfHoverOver.viewModel.children.length - 2].id;
+            }
+          }
         }
-        c.l(`dragNDrop.lastChildViewModelId: ${dragNDrop.lastChildViewModelId}`);
-      } else {
+      } else { // NOTE: 10-03-2017: The position is not middle
         parentOfHoverOver = graphTraversal.findParent(stateNew, dragNDrop.hoverOverId);
         dragNDrop.parentOfHoverOverId = parentOfHoverOver.id;
         const hoverChildsIndex = graphTraversal.getChildsIndex(parentOfHoverOver.viewModel.children, dragNDrop.hoverOverId);
@@ -53,30 +71,6 @@ class DndActionHandler {
 
     return stateNew;
   }
-
-  // static moveAsPhantom(state, parentOfHover) {
-  //   const dragNDrop = state.dragNDrop;
-  //
-  //   let phantomDropper;
-  //   if (!!dragNDrop.phantomDropperId) {
-  //     phantomDropper = graphTraversal.find(state, dragNDrop.phantomDropperId);
-  //   } else {
-  //     const model = phantomDropperModelFactory.createInstance(parentOfHover.viewModel.id, f22Uuid.generate());
-  //     phantomDropper = viewModelFactory.generateViewModel(parentOfHover.id, model);
-  //     dragNDrop.phantomDropperId = phantomDropper.id;
-  //   }
-  //
-  //   if (!!phantomDropper) {
-  //     const parentOfDropper = graphTraversal.find(state, phantomDropper.parentId);
-  //     parentOfDropper.viewModel.children = parentOfDropper.viewModel.children.filter(child => child.id !== phantomDropper.id);
-  //     phantomDropper.parentId = parentOfHover.id;
-  //   }
-  //
-  //   parentOfHover.viewModel.children.splice(dragNDrop.indexChildTarget, 0, phantomDropper);
-  //
-  //   /* eslint-disable no-param-reassign */
-  //   parentOfHover.viewModel.children = [].concat(parentOfHover.viewModel.children);
-  // }
 }
 
 export default DndActionHandler;
